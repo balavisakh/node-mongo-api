281ec6e73a227803b83af7771083c20662c9298f
unknown <balavisakh@gmail.com>
2020-07-29 14:00:12 +0530

    included crud api

--- controllers/user.js
+++ controllers/user.js
@@ -15,21 +15,47 @@ router.route('/').get((req,res) => {
     });
 });
 
-// router.route('/').post((req,res) => {
-//     const model = new userModel({
-//         name: req.body.name,
-//         age: req.body.age,
-//         phone: req.body.phone
-//     });
-//     try {
-//         const user = model.save();
-//         res.json(user);
-//     }
-//     catch(err) {
-//         res.send('error',err);
-//     }
-// });
+// Add user details to mongodb
+router.route('/adduser').post((req,res) => {
+    const model = new userModel(req.body);
+    try {
+        const newuser = model.save().then(() => {
+        res.status(200).json({'user':'new user added'});
+        });
+    }
+    catch(err) {
+        res.send('error',err);
+    }
+});
 
+// Update user details in mongodb
+router.route('/updateuser/:id').put((req,res) => {
+    userModel.findById(req.params.id, (err,user) => {
+        try {
+            user.name = req.body.name;
+            user.age = req.body.age;
+            user.phone = req.body.phone;
+            user.save().then(() => {
+            res.status(200).json({'user':'user detail updated'});
+            });
+        }
+        catch {
+            res.send('update failed', err);
+        }
+    });
+});
 
-// exporting this file so we can is it outside
+// Delete user in mongodb
+router.route('/deleteuser/:id').delete((req,res) => {
+    userModel.findByIdAndRemove({ _id: req.params.id }, (err,user) => {
+        try {
+            res.status(200).json({'user': 'user deleted'});
+        }
+        catch {
+            res.send('failed', err);
+        }
+    })
+})
+
+// exporting this file so we can use is it in another file
 module.exports = router;
